a1a9e9508eaf8ffac1238be7f7dbbf72374039a2
msx98 <me@glub.xyz>
2024-04-16 14:58:34 +0300

    more reliable status message

--- MessageFetch/app/fetch_channel_history.py
+++ MessageFetch/app/fetch_channel_history.py
@@ -129,24 +129,6 @@ def get_channel_counts(
     return s
 
 
-def get_status(
-        d_dialogs: Dict[int, Dialog],
-        d_counts: Dict[int, int],
-        pending: List[int],
-        finished: List[int],
-        current: int
-) -> str:
-    sum_counts = sum([v for k, v in d_counts.items() if k in finished]) + (d_counts[current] if current is not None else 0)
-    s = ""
-    s += "Pending:\n"
-    s += get_channel_counts(d_dialogs, d_counts, [current] + pending) + "\n\n"
-    if current is not None:
-        s += f"""Currently working on "{d_dialogs[current].chat.title}"\n"""
-    s += f"""Finished {len(finished)}, pending {len(pending)}, total {len(d_dialogs)}\n"""
-    s += f"""Total messages: {sum_counts}"""
-    return s
-
-
 async def get_dialogs() -> Dict[int, Dialog]:
     my_dialogs = app.get_dialogs()
     d: Dict[int, Dialog] = dict()
@@ -158,59 +140,136 @@ async def get_dialogs() -> Dict[int, Dialog]:
 from mongo_backend import StoredDialog
 
 
-async def main():
-    channel_counts: Dict[int, int] = dict()
-    pending: List[int] = []
-    finished: List[int] = []
-    status_msg: Message = await app.send_message(DEBUG_CHAT_ID, "Starting up")
-    current_dialogs: Dict[int, Dialog] = await get_dialogs()
+class StatusMessage:
+    def __init__(self, update_interval: float = 5):
+        self.message = None
+        self.config = dict()
+        self.update_interval = update_interval
+        self.last_s = None
+        self.announce_update()
     
-    result = db.delete_last_write()
-    if result is not None:
-        channel_id, channel_name, delete_count = result
-        await app.send_message(DEBUG_CHAT_ID, f"Deleted {delete_count} messages from the write attempt to {channel_name}")
+    async def start(self):
+        assert self.message is None
+        self.message = await app.send_message(DEBUG_CHAT_ID, "Starting up")
+    
+    def announce_update(self):
+        self.last_update = time.time()
+        self.next_update = self.last_update + self.update_interval
+    
+    async def update(self, **kwargs):
+        self.config |= kwargs
+        if time.time() >= self.next_update:
+            try:
+                await self._update(**self.config)
+                self.announce_update()
+            except FloodWait as e:
+                print(f"Got floodwait with {e.value}")
+                self.next_update = time.time() + float(e.value) + self.update_interval
+            except Exception as e:
+                print(f"Got exception {e}")
+                self.next_update = time.time() + self.update_interval
+    
+    async def announce_recover(self):
+        await self.update(last_recover=time.time())
+    
+    async def announce_finish(self):
+        await self.update(last_finish=time.time())
+    
+    async def _update(
+            self,
+            *,
+            d_dialogs: Dict[int, Dialog] = dict(),
+            d_counts: Dict[int, int] = dict(),
+            pending: List[int] = [],
+            finished: List[int] = [],
+            current: int = None,
+            kicked: List[str] = [],
+            last_finish: float = None,
+            last_recover: float = None,
+            **kwargs,
+    ):
+        sum_counts = 0
+        if d_counts:
+            sum_counts += sum([v for k, v in d_counts.items() if k in finished])
+            sum_counts += d_counts[current] if current is not None else 0
+        s = ""
+        if last_finish is not None:
+            s += f"""Last finish: {datetime.datetime.fromtimestamp(last_finish).strftime('%H:%M:%S')}\n"""
+        if last_recover is not None:
+            s += f"""Last recover: {datetime.datetime.fromtimestamp(last_recover).strftime('%H:%M:%S')}\n"""
+        if kicked:
+            sep = '\n'
+            s += f"""Kicked from {len(kicked)} channels:\n{sep.join(kicked)}\n\n"""
+        if (current is not None) or pending:
+            s += "Pending:\n"
+            s += get_channel_counts(d_dialogs, d_counts, ([current] if current is not None else []) + (pending or [])) + "\n\n"
+        if current is not None:
+            s += f"""Currently working on "{d_dialogs[current].chat.title}"\n"""
+        if finished or pending or d_dialogs:
+            s += f"""Finished {len(finished)}, pending {len(pending)}, total {len(d_dialogs)}\n"""
+        if sum_counts:
+            s += f"""Total messages: {sum_counts}\n"""
+        s += "Updated " + datetime.datetime.now().strftime("%H:%M:%S") + "\n"
+        s = s.strip()
+        if s:
+            if s != self.last_s:
+                await self.message.edit_text(s)
+                self.last_s = s
+            else:
+                pass
+        else:
+            print(f"No channels yet")
+    
+    async def close(self):
+        await self.message.delete()
+        self.message = None
 
-    stored_dialogs: Dict[int, StoredDialog] = db.get_stored_dialogs()
-    kicked_channels = [v.title for k,v in stored_dialogs.items() if k not in current_dialogs.keys()]
-    if kicked_channels:
-        newl = "\n"
-        await app.send_message(DEBUG_CHAT_ID, f"""Sleeping because kicked from {len(kicked_channels)} channels:{newl}{(","+newl).join(kicked_channels)}""")
-        time.sleep(60)
-    all_dialogs = {k:dialog for k,dialog in current_dialogs.items() if dialog.chat.type == ChatType.CHANNEL}
-    await status_msg.edit_text(f"Got {len(all_dialogs)} channels, checking which ones are already fetched")
-    id_to_max_message: Dict[int, int] = {k: v.max_id for k, v in stored_dialogs.items() if k in all_dialogs.keys()}
-    id_to_leftovers: Dict[int, int] = {dialog.chat.id: ((dialog.top_message.id if dialog.top_message else -1) - id_to_max_message.get(dialog.chat.id,-1)) for dialog in all_dialogs.values()}
-    sorted_channels = sorted(id_to_leftovers.items(), key=lambda x: x[1], reverse=False) # sort in ascending order
-    sorted_channels_with_names = [(x[0], all_dialogs[x[0]].chat.title, x[1]) for x in sorted_channels]
-    all_channels = [x[0] for x in sorted_channels if x[0] in all_dialogs.keys()]
-    pending = [x for x in all_channels if id_to_leftovers[x] > 0]
-    finished = [x for x in all_channels if x not in pending]
-    channel_counts = {channel_id: 0 for channel_id in pending}
-    while pending:
-        channel_id = pending.pop(0)
-        assert channel_id is not None
-        db.select_channel(all_dialogs[channel_id])
-        next_possible = time.time() + 0
-        async for row in client.get_chat_history(channel_id):
-            if row.id <= id_to_max_message.get(channel_id, -1):
-                break
-            db.add_message(row)
-            channel_counts[channel_id] += 1
-            if time.time() >= next_possible:
-                try:
-                    await status_msg.edit_text(get_status(current_dialogs, channel_counts, pending, finished, channel_id))
-                    next_possible = time.time() + 10
-                except FloodWait as e:
-                    print(f"Got floodwait with {e.value}")
-                    next_possible = time.time() + float(e.value) + 1
-                    continue
-                except Exception as e:
-                    print(f"Got exception {e}")
-                    next_possible = time.time() + 10
-                    pass
-        finished.append(channel_id)
-        db.unselect_channel()
-    await status_msg.edit_text(get_status(current_dialogs, channel_counts, pending, finished, None))
+
+async def main():
+    status_msg = StatusMessage()
+    await status_msg.start()
+
+    while True:
+        channel_counts: Dict[int, int] = dict()
+        pending: List[int] = []
+        finished: List[int] = []
+        current_dialogs: Dict[int, Dialog] = await get_dialogs()
+        await status_msg.update(d_dialogs=current_dialogs, d_counts=channel_counts, pending=pending, finished=finished, current=None)
+        
+        result = db.delete_last_write()
+        if result is not None:
+            channel_id, channel_name, delete_count = result
+            print(f"Deleted {delete_count} messages from the write attempt to {channel_name}")
+            await status_msg.announce_recover()
+
+        stored_dialogs: Dict[int, StoredDialog] = db.get_stored_dialogs()
+        kicked_channels = [v.title for k,v in stored_dialogs.items() if k not in current_dialogs.keys()]
+        await status_msg.update(kicked=kicked_channels)
+        all_dialogs = {k:dialog for k,dialog in current_dialogs.items() if dialog.chat.type == ChatType.CHANNEL}
+        await status_msg.update(d_dialogs=all_dialogs)
+        id_to_max_message: Dict[int, int] = {k: v.max_id for k, v in stored_dialogs.items() if k in all_dialogs.keys()}
+        id_to_leftovers: Dict[int, int] = {dialog.chat.id: ((dialog.top_message.id if dialog.top_message else -1) - id_to_max_message.get(dialog.chat.id,-1)) for dialog in all_dialogs.values()}
+        sorted_channels = sorted(id_to_leftovers.items(), key=lambda x: x[1], reverse=False) # sort in ascending order
+        sorted_channels_with_names = [(x[0], all_dialogs[x[0]].chat.title, x[1]) for x in sorted_channels]
+        all_channels = [x[0] for x in sorted_channels if x[0] in all_dialogs.keys()]
+        pending += [x for x in all_channels if id_to_leftovers[x] > 0]
+        finished += [x for x in all_channels if x not in pending]
+        channel_counts |= {channel_id: 0 for channel_id in pending}
+        await status_msg.update()
+        while pending:
+            channel_id = pending.pop(0)
+            assert channel_id is not None
+            await status_msg.update(current=channel_id)
+            db.select_channel(all_dialogs[channel_id])
+            async for row in client.get_chat_history(channel_id):
+                if row.id <= id_to_max_message.get(channel_id, -1):
+                    break
+                db.add_message(row)
+                channel_counts[channel_id] += 1
+                await status_msg.update()
+            finished.append(channel_id)
+            db.unselect_channel()
+        await status_msg.announce_finish()
 
 
 #@app.on_message(filters.private & filters.command("status"))
